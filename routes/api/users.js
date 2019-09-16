efacb35a6ccf058608cd195b9a297c1fb7dc739a
swapneal <52267392+swapneal@users.noreply.github.com>
2019-09-15 20:06:02 -0700

    Hash password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -8,7 +9,9 @@ const User = require('../../models/user');
 // @desc User register
 // @access public route
 router.post('/register', (req, res) => {
-  User.findOne({ username: req.body.username })
+  User.findOne({
+      username: req.body.username
+    })
     .then(user => {
       if (user) {
         return res.status(400).json({
@@ -21,10 +24,37 @@ router.post('/register', (req, res) => {
           password: req.body.password,
           email: req.body.email
         });
-        return res.status(200).json({ msg: 'success' });
+        // return res.status(200).json({ msg: 'success' });
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save().then(user => res.json(user)).catch(err => console.log('Error in password hash: ' + err));
+          });
+        });
       }
     })
-    .catch(err => console.log('Unique Username Check Error Msg: '+ err))
+    .catch(err => console.log('Unique Username Check Error Msg: ' + err))
 });
+
+// @route POST api/users/login
+// @desc User Login page
+// @access public route
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    username
+  }).then(user => {
+    if (!user) {
+      return res.status(404).json({
+        msg: 'Invalid Username'
+      })
+    }
+  }).catch(err => console.log('Error in username: ' + err))
+})
+
 
 module.exports = router;
